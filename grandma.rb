949463d10baed56d4fb4ed7da7ec840d2b16e631
Brett Weisberg <weisberg.brett@gmail.com>
2018-02-23 19:15:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-    if phrase.upcase == phrase
+    if phrase.upcase == phrase && phrase == "I LOVE YOU GRANDMA!"
+      return  "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase.upcase == phrase
       return "NO, NOT SINCE 1938!"
-    elsif phrase.upcase == phrase && phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
     else
       return "HUH?! SPEAK UP, SONNY!"
     end
